3b0dea56ea21aa446476b2a27f678ac4424acac6
D4NGK4 <danjosheps.djt98@gmail.com>
2023-01-13 22:00:59 +0800

    commit 1st

---
+++ OOP_Project_Library/src/main/java/com/mycompany/oop_project_library/OOP_Project_Library.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop_project_library;
+
+/**
+ *
+ * @author dan
+ */
+public class OOP_Project_Library {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    hello
+}
